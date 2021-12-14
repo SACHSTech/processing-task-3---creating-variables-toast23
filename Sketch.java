29485f5676b8c56c02a5c8a43fbd8227ffe28d20
tyao2 <tony.yao23@ycdsbk12.ca>
2021-12-14 16:00:55 -0500

    finished task 3

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,61 @@
 import processing.core.PApplet;
+import java.util.Random;
+
 
 public class Sketch extends PApplet {
+
+  Random rand = new Random();
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  // width and height 
+	float width = 800f;
+  float height = 700f;
+
+  // scale factor
+  float wScaleF = width / 400f;
+  float hScaleF = height / 400f;
+
+
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size((int)width, (int)height);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(0, 255, 0);
+    noLoop();
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	// sky 
+  fill(0, 255, 255);
+  rect(0f, 0f, width, 230f * hScaleF);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // house 
+  fill(255, 205, 180);
+  rect(170f * wScaleF, 220f * hScaleF, 70 * wScaleF, 45 * hScaleF);
+
+  // roof
+  fill(165, 42, 42);
+  triangle(170 * wScaleF, 220 * hScaleF, 205 * wScaleF, 195 * hScaleF, 240 * wScaleF, 220 * hScaleF);
+
+  // window
+  fill(0, 0, 255);
+  rect(179 * wScaleF, 232 * hScaleF, 11 * wScaleF, 12 * hScaleF);
+  rect(220 * wScaleF, 232 * hScaleF, 11 * wScaleF, 12 * hScaleF);
+
+  // door 
+  fill(165, 42, 42);
+  rect(200 * wScaleF, 242 * hScaleF, 10 * wScaleF, 20 * hScaleF);
+
+  // sun 
+  fill(255, 255, 0);
+  ellipse(rand.nextInt((int)width), (rand.nextInt((int)(190f * hScaleF))), 80 * wScaleF, 80 * hScaleF);
+
+  // time 
+  fill(0, 0, 0);
+  text(year() + "-" + month() + "-" + day(), 0, height);
+
   }
-  
-  // define other methods down here.
+
+
 }
